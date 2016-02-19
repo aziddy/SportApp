80cf6e020f5a54e36d8bb794ae1a145c40565e1e
aziddy <alexander.zidros@gmail.com>
2016-02-19 00:18:55 -0800

    uhhhh

--- app/src/main/java/layout/PickupCompletedFragment.java
+++ app/src/main/java/layout/PickupCompletedFragment.java
@@ -1,17 +1,20 @@
 package layout;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.alexander.sportapp.R;
 
 
-public class PickupCompletedFragment extends Fragment {
+public class PickupCompletedFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
@@ -20,14 +23,31 @@ public class PickupCompletedFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+              //  Lenovo U530
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pickup_completed, container, false);
+
+        Button clickBtn = (Button) view.findViewById(R.id.clickBtn);
+
+        clickBtn.setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pickup_completed, container, false);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
 
+        if(v.getId() == R.id.clickBtn){
+
+            Toast.makeText(getContext(), "wtf it worked the listener", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
 }
